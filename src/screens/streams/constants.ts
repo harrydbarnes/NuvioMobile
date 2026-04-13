d2e92b3ee61b22b927837c109acbf0d684ea4163
harrydbarnes <145344818+harrydbarnes@users.noreply.github.com>
2026-04-13 09:35:16 +0000

    ⚡ Bolt: Clickable local IMDb logo

--- src/screens/streams/constants.ts
+++ src/screens/streams/constants.ts
@@ -7,7 +7,7 @@ export const SCREEN_HEIGHT = height;
 
 // Image URLs
 export const TMDB_LOGO = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tmdb.new.logo.svg/512px-Tmdb.new.logo.svg.png?20200406190906';
-export const IMDb_LOGO = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png';
+export const IMDb_LOGO = require('../../../assets/rating-icons/imdb.png');
 export const HDR_ICON = 'https://uxwing.com/wp-content/themes/uxwing/download/video-photography-multimedia/hdr-icon.png';
 export const DOLBY_ICON = 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/Dolby_Vision_%28logo%29.svg/512px-Dolby_Vision_%28logo%29.svg.png?20220908042900';
 
